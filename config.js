eff017010b3ee0cdb5fc6ac6966b3eb01d83fc0c
Jurgen Mane <jurgenmane@gmail.com>
2021-05-02 17:48:40 -0500

    NodeJS, Express, MongoDB Workshop Week 4

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '2506504289496412',
+        clientSecret: '0b079e30d76b3a60e229d8c5f3f6c948'
+    }
 }
